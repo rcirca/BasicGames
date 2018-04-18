308238529130228ce2fd6c6fcddd205ca7063f75
robert echeverria <recheverria@bluebeam.com>
2018-04-18 16:46:47 -0700

    Partial implementation of StateManager.

--- SFMLGames/SFMLGames/StateManager.cpp
+++ SFMLGames/SFMLGames/StateManager.cpp
@@ -1,19 +1,51 @@
 #include "StateManager.h"
 
-StateManager::StateManager(SharedContext * pShared) : m_shared(pShared)
+StateManager::StateManager(SharedContext * pShared) : _shared(pShared)
 {
+	RegisterState<State_Intro>(StateType::Intro);
+	RegisterState<State_MainMenu>(StateType::MainMenu);
+	RegisterState<State_Game>(StateType::Game);
+	RegisterState<State_Paused>(StateType::Paused);
 }
 
 StateManager::~StateManager()
 {
+	for (auto &itr : _states)
+	{
+		itr.second->onDestroy();
+		delete itr.second;
+	}
 }
 
 void StateManager::Update(const sf::Time & pTime)
 {
+
 }
 
 void StateManager::Draw()
 {
+	if (_states.empty())
+		return;
+
+	if (_states.back().second->isTransparent()
+		&& _states.size() > 1)
+	{
+		auto itr = _states.end();
+		while (itr != _states.begin())
+		{
+			if (itr != _states.end())
+			{
+				if (!itr->second->isTransparent())
+					break;
+			}
+			--itr;
+		}
+
+		for (; itr != _states.end(); ++itr)
+			itr->second->draw();
+	}
+	else
+		_states.back().second->draw();
 }
 
 void StateManager::ProcessRequests()
