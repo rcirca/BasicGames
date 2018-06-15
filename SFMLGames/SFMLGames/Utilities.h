473aab728dfc313fabfc3482b771bedcacc2965e
recheverria <failquality@gmail.com>
2018-06-14 23:10:51 -0700

    Add util class to grab fielpath to the location of the exe for easier acces in finding additional resource files

---
+++ SFMLGames/SFMLGames/Utilities.h
@@ -0,0 +1,38 @@
+#pragma once
+#define RUNNING_WINDOWS
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+namespace Utils
+{
+#ifdef RUNNING_WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include <Shlwapi.h>
+#include <windows.h>
+	inline std::string getWorkingDirectory()
+	{
+		auto hModule = GetModuleHandle(nullptr);
+		if(hModule)
+		{
+			char path[256];
+			GetModuleFileName(hModule, path, sizeof(path));
+			PathRemoveFileSpec(path);
+			strcat_s(path, "\\");
+			return std::string(path);
+		}
+		return "";
+	}
+#elif defined RUNNING_LINUX
+#include <unistd.h>
+	inline std::string getWorkingDirectory()
+	{
+		char cwd[1024];
+		if(getcwd(cwd, sizeof(cwd)) != nullptr)
+			return std::string(cwd) + std::string("/");
+
+		return "";
+	}
+#endif
+
+}
